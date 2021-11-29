c6a87a582b6a94fced703533c9a5b586e9b3ce3c
Abdul2067 <abdul2067@gmail.com>
2021-11-28 19:35:15 -0500

    finalize delete function & edit

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
   path("travels/", views.travels_index, name="travels_index"),
   path("travels/<int:travel_id>/", views.travels_detail, name="travels_detail"),
   path("travels/create/", views.TravelCreate.as_view(), name="travels_create"),
-  path("travels/<int:pk>/update", views.TravelUpdate.as_view(), name="travels_update"),
-  path("travels/<int:pk>/delete", views.TravelDelete.as_view(), name="travels_delete"),
+  path("travels/<int:pk>/update/", views.TravelUpdate.as_view(), name="travels_update"),
+  path("travels/<int:pk>/delete/", views.TravelDelete.as_view(), name="travels_delete"),
 ]
