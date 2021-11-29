f16916095cf20e4363e9c0d9111de44b59faed05
Abdul2067 <abdul2067@gmail.com>
2021-11-28 21:33:04 -0500

    add activity view in travel detail

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Travel
+from .models import Activity, Travel
 
 # Register your models here.
 
 admin.site.register(Travel)
+admin.site.register(Activity)
